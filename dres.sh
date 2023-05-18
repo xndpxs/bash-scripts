9049ed035d8aa0d658a7a218260ddfef882dd7b3
xndpxs <xndpxs@yahoo.es>
2023-05-17 19:53:57 -0500

    added gnome-randr-rust for gnome wayland

--- dres.sh
+++ dres.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+
+if [[ $XDG_CURRENT_DESKTOP == "KDE" ]]; then
+
 get_knumber=$(kscreen-doctor -o | grep @240 | awk '{print $2}' | sed 's/\x1B\[[0-9;]*[mG]//g')
 kscreen-doctor output.$get_knumber.mode.1920x1080@100
 
+else
+
+get_gnumber=$(gnome-randr query | grep -B 2 "@240" | awk 'NR==2{print $1}'|sed 's/\x1B\[[0-9;]*[mG]//g')
+gnome-randr modify $get_gnumber --mode 1920x1080@99.930
+
+fi
