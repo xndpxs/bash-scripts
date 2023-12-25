f7054123b40f854bfa088fdd9138e047d17aab83
xndpxs <xndpxs@outlook.com>
2023-12-25 11:44:29 -0500

    added qemu.conf

--- virtualization_arch.sh
+++ virtualization_arch.sh
@@ -16,7 +16,22 @@ sudo cp /etc/libvirt/qemu.conf /etc/libvirt/qemu.conf.bak
 sudo sed -i "/#user = \"username\"/c\user = \"$USER\"" /etc/libvirt/qemu.conf
 sudo sed -i "/#group = \"username\"/c\group = \"$USER\"" /etc/libvirt/qemu.conf
 
-# enable y start
+# Habilita e inicia el servicio libvirtd
 sudo systemctl enable libvirtd.service
 sudo systemctl start libvirtd.service
 sudo systemctl restart libvirtd.service
+
+# Verifica si el archivo libvirtd.conf necesita ser editado
+if ! grep -q "^unix_sock_group = \"libvirt\"" /etc/libvirt/libvirtd.conf; then
+    sudo sed -i "/#unix_sock_group =/c\unix_sock_group = \"libvirt\"" /etc/libvirt/libvirtd.conf
+fi
+
+if ! grep -q "^unix_sock_rw_perms = \"0770\"" /etc/libvirt/libvirtd.conf; then
+    sudo sed -i "/#unix_sock_rw_perms =/c\unix_sock_rw_perms = \"0770\"" /etc/libvirt/libvirtd.conf
+fi
+
+# Reinicia el servicio libvirtd después de realizar los cambios
+sudo systemctl restart libvirtd.service
+
+# Verifica el estado del servicio
+systemctl status libvirtd.service
